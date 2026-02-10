ce42768c23c36e931d738f7b1cfafc9e4ff1272a
SUN4273 <188133840+SUN4273@users.noreply.github.com>
2026-02-10 00:21:31 +0000

    Day-1(Basics) Of javaScript

---
+++ 01_Basics.js
@@ -0,0 +1,33 @@
+const value = 10;
+let msg;
+var message = "Hello";
+var Message = "World";
+let name = "Sun" , /* Declare two or more variable seperate each 
+                     variable using comma*/
+    count = 10;
+
+let Name = "Hello Sunny"; // Varibales are case sensitive.
+Name = "Hello Pie"; // We also update the variable after assigning it.
+
+
+
+/*########################  Here are the ways to print Variables########## */
+
+console.log(Name); 
+// console.log(Name,name,count); // Here we print all the variable in a same console.log()
+// console.log(name,count); // we also print multiple variables using comma.
+// console.log(message,Message); // This is Concatinaton
+// value = 20; // This is not allowed because this is constant 
+console.log(value);
+console.log(msg); // If we declare a variable but not initialized the value it shows "undefined"
+
+
+
+
+
+
+
+
+
+
+
