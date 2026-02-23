03a6035c111e4808252018494642008d721178c3
SUN4273 <188133840+SUN4273@users.noreply.github.com>
2026-02-23 10:55:53 +0000

    practice of JAVA

---
+++ javaPr/01_day.js
@@ -0,0 +1,39 @@
+// console.log(typeof 43); // return num.
+// console.log(typeof "Sun"); // return string.
+// console.log(typeof true); // retrun boolean.
+// console.log(typeof null); // returns obj.
+// console.log(typeof NaN); // This is also the number.
+// console.log(typeof undefined); // returns undefined.
+
+
+console.log(5 == "5"); // true
+console.log(5 === "5"); // false whyyyy?
+
+
+// console.log(null == undefined); // true
+// console.log(null === undefined); // false
+
+let str = "Name Sunny";
+// console.log(str.length);
+
+
+let msg = 'I\' am learning javaScript';
+let msg2 = `I am sunny and I live in Delhi`;
+
+// console.log(msg);
+// console.log(msg2);
+
+let a1;
+let b1 = null;
+
+// console.log(a1); // undefined
+// console.log(b1); // null
+// console.log(typeof a1); // undefined
+// console.log(typeof b1); // object
+
+
+let cel = 100;
+message = (cel * 9/5) + 32;
+console.log(`At ${cel} degree, The Farenheit is ${message}`);
+
+
